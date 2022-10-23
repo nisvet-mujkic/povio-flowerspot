ef1797da96d3f4ddb43dd220f397800809444616
Nisvet Mujkić <nisvet.mujkic@outlook.com>
2022-10-23 22:30:41 +0200

    validations

---
+++ Povio.FlowerSpot.Application.UnitTests/Features/Users/Validators/RegisterUserCommandValidatorShould.cs
@@ -0,0 +1,46 @@
+﻿using FluentValidation.TestHelper;
+using Povio.FlowerSpot.Application.Features.Users.Commands.Register;
+using Povio.FlowerSpot.Application.Features.Users.Validators.Register;
+using Xunit;
+
+namespace Povio.FlowerSpot.Application.UnitTests.Features.Users.Validators
+{
+    public class RegisterUserCommandValidatorShould
+    {
+        private readonly RegisterUserCommandValidator _validator = new();
+
+        [Theory]
+        [InlineData("test", "emailaddress.com", "test")]
+        [InlineData("test:test", "emailaddress.com", "test:test")]
+        public void HaveErrorsIfInputDataIsInvalid(string username, string email, string password)
+        {
+            var validationResult = _validator.TestValidate(new RegisterUserCommand(username, password, email));
+
+            validationResult.ShouldHaveValidationErrorFor(x => x.Username);
+            validationResult.ShouldHaveValidationErrorFor(x => x.Email);
+            validationResult.ShouldHaveValidationErrorFor(x => x.Password);
+        }
+
+        [Theory]
+        [InlineData("nisvetmujkic", "nisvetmujkic")]
+        public void HaveErrorsIfUsernameAndPasswordAreEqual(string username, string password)
+        {
+            var validationResult = _validator.TestValidate(new RegisterUserCommand(username, password, "email@address.com"));
+
+            validationResult.ShouldNotHaveValidationErrorFor(x => x.Username);
+            validationResult.ShouldNotHaveValidationErrorFor(x => x.Email);
+            validationResult.ShouldHaveValidationErrorFor(x => x.Password);
+        }
+
+        [Theory]
+        [InlineData("nisvetmujkic", "nisvet.mujkic@gmail.com", "nisvet2022")]
+        public void NotHaveErrorsInputDataIsValid(string username, string email, string password)
+        {
+            var validationResult = _validator.TestValidate(new RegisterUserCommand(username, password, email));
+
+            validationResult.ShouldNotHaveValidationErrorFor(x => x.Username);
+            validationResult.ShouldNotHaveValidationErrorFor(x => x.Email);
+            validationResult.ShouldNotHaveValidationErrorFor(x => x.Password);
+        }
+    }
+}
---
+++ Povio.FlowerSpot.Application/Features/Users/Validators/Register/RegisterUserCommandValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using Povio.FlowerSpot.Application.Features.Users.Commands.Register;
+
+namespace Povio.FlowerSpot.Application.Features.Users.Validators.Register
+{
+    public class RegisterUserCommandValidator : AbstractValidator<RegisterUserCommand>
+    {
+        public RegisterUserCommandValidator()
+        {
+            RuleFor(x => x.Username)
+                .NotEmpty()
+                .Length(6, 20)
+                .Must(x => !x.Contains(':'));
+
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .Length(6, 100)
+                .Must((command, password) => !password.Contains(':') && password != command.Username);
+
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .EmailAddress();
+        }
+    }
+}
